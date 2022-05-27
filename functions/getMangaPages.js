a92d9524e665224e2af5caa3510003b82a69200b
Vinicius Prado <vinicavagna@gmail.com>
2022-05-27 14:01:29 -0300

    :sparkles: added function to download all chapter images - TODO: think about folder structure - TODO: make the application idempotent

--- functions/getMangaPages.js
+++ functions/getMangaPages.js
@@ -1,4 +1,126 @@
 const withBrowser = require("./withBrowser");
+const fetch = require("node-fetch");
+const path = require("path");
+const fs = require("fs-extra");
+const { Page } = require("puppeteer");
+
+const downloadBase64 = async (page, downloadUrl) => {
+  console.log(downloadUrl);
+
+  await new Promise((resolve) => setTimeout(resolve, 1000000));
+
+  const base64 = await page.evaluate(async (url) => {
+    const arrayBufferToBase64 = (buffer) => {
+      let binary = "";
+
+      const bytes = new Uint8Array(buffer);
+
+      const len = bytes.byteLength;
+
+      for (let i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
+      }
+
+      return window.btoa(binary);
+    };
+
+    const response = await fetch(url);
+
+    const bufferArr = await response.arrayBuffer();
+
+    const base64 = arrayBufferToBase64(bufferArr);
+
+    return base64;
+  }, downloadUrl);
+
+  return base64;
+};
+
+const getChaptersLinks = async (page) => {
+  await page.waitForSelector(".row-content-chapter");
+  const links = await page.evaluate(() => {
+    const anchors = Array.from(
+      document.querySelectorAll(".row-content-chapter > .a-h > a")
+    );
+
+    const links = anchors.map((item) => item.href).slice(0, 1);
+
+    return links.reverse();
+  });
+
+  return links;
+};
+
+/**
+ *
+ * @param {*} link
+ * @param {Page} page
+ */
+const gotToChapterPageAndDownloadImages = async (link, page) => {
+  await page.setRequestInterception(true);
+
+  const MANGA_PATH = path.join(__dirname, "..", "teste/");
+
+  await fs.mkdirp(MANGA_PATH);
+
+  page.on("response", async (response) => {
+    if (/img/.test(response.url())) {
+      const buffer = await response.buffer();
+
+      const base = path.basename(response.url());
+
+      const filepath = path.join(MANGA_PATH, base);
+
+      await fs.promises.writeFile(filepath, buffer);
+    }
+  });
+
+  page.on("request", (request) => {
+    request.continue();
+  });
+
+  await page.goto(link);
+
+  await new Promise((resolve) => setTimeout(resolve, 100000));
+};
+
+const getMangaChaptersImages = async (links, browser) => {
+  const promises = links.map(async (link) => {
+    const page = await browser.newPage();
+    await gotToChapterPageAndDownloadImages(link, page);
+  });
+
+  await Promise.all(promises);
+};
+
+const getMangaName = async (page) => {
+  const mangaNameSelector = ".story-info-right > h1";
+
+  await page.waitForSelector(mangaNameSelector);
+  const element = await page.$(mangaNameSelector);
+
+  const mangaName = await element.evaluate((el) => el.textContent);
+
+  return mangaName;
+};
+
+/**
+ *
+ * @param {string} dirPath
+ * @returns {Promise<boolean>}
+ */
+export const checkDirectoryExists = async (dirPath) => {
+  try {
+    const stat = await fs.promises.stat(dirPath);
+
+    return stat.isDirectory();
+  } catch (err) {
+    if (err && err.code == "ENOENT") {
+      return false;
+    }
+    throw err;
+  }
+};
 
 const getMangaPages = async (manga) => {
   await withBrowser(async (browser, page) => {
@@ -18,23 +140,20 @@ const getMangaPages = async (manga) => {
       return links.shift().href;
     });
 
-    console.log(link);
-
     await page.goto(link);
 
-    await page.waitForSelector(".row-content-chapter");
+    const mangaName = await getMangaName(page);
 
-    const links = await page.evaluate(() => {
-      const anchors = Array.from(
-        document.querySelectorAll(".row-content-chapter > .a-h > a")
-      );
+    console.log({ mangaName });
 
-      const links = anchors.map((item) => item.href);
+    const links = await getChaptersLinks(page);
 
-      return links.reverse();
-    });
-
-    console.log({ links });
+    /** TODO: Check if folder already exists */
+    const mangaPath = path.join(__dirname, "..", "mangas", `${mangaName}`);
+    const directoryAlreadyExists = await checkDirectoryExists(mangaPath);
+    while (links.length) {
+      await getMangaChaptersImages(links.splice(0, 10), browser);
+    }
 
     await new Promise((resolve) => setTimeout(resolve, 10000));
   });
